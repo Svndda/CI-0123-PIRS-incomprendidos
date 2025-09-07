c8c1d773590ca3bf7825bfb66e6af0a9aab83a14
Aaron Sanchez <aaroncz032@gmail.com>
2025-09-07 17:56:56 -0600

    chore: Contantes de colores creadas.

---
+++ SafeSpace/client/src/colors.h
@@ -0,0 +1,14 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#include <QString>
+
+#endif // COLORS_H
+
+namespace Colors {
+  const QString DeepBlue = "#4044ED";
+  const QString LigthBlue = "#93DFFF";
+  const QString White = "#FFFFFF";
+  const QString Gray = "#999999";
+  const QString Black = "#27262C";
+}
