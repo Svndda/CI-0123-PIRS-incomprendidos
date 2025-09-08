49c1c11f6525cf36a8b77718ec1719a7bdb9ac61
Aaron Sanchez <aaroncz032@gmail.com>
2025-09-07 18:00:51 -0600

    chore: Clase base de Usuario
    - Creacion de la clase base para la representacion de un usuario.

---
+++ SafeSpace/client/src/model/user.cpp
@@ -0,0 +1,103 @@
+#include "user.h"
+
+User::User(const size_t userId, const std::string userName
+    , const std::vector<PageAccess> userPermissions)
+    : id(userId)
+    , name(userName)
+    , permissions(userPermissions) {
+}
+
+User::PageAccess::PageAccess(const size_t pageIndexed, const size_t pageAccess)
+    : pageIndex(pageIndexed)
+    , access(pageAccess) {
+}
+
+const size_t& User::getID() const {
+  // Returns the user's id.
+  return this->id;
+}
+
+void User::setUserID(const size_t id) {
+  // Sets the user's id.
+  this->id = id;
+}
+
+const std::string& User::getUsername() const {
+  // Returns the user's name.
+  return this->name;
+}
+
+void User::setUsername(const std::string name) {
+  // Sets the user's name.
+  this->name = name;
+}
+
+void User::setPassword(const std::string& newPassword) {
+  // Creates a std hasher for the given password.
+  std::hash<std::string> hasher;
+  // Sets the hashed result as the user's password.
+  this->password = hasher(newPassword);
+}
+
+bool User::verifyPassword(const std::string& passwordToCheck) const {
+  // Creates a std hasher for the given password.  
+  std::hash<std::string> hasher;
+  // Checks if the hashed result equals the user's password.
+  return this->password == hasher(passwordToCheck);
+}
+
+const std::vector<User::PageAccess>& User::getUserPermissions() const {
+  // Returns the user's permissions.
+  return this->permissions;
+}
+
+void User::setUserPermissions(const std::vector<User::PageAccess> permissions) {
+  // Sets the user's permissions.
+  this->permissions = permissions;
+}
+
+bool User::operator==(const User& other) const {
+  // Checks if the users's attributes matches.
+  return (this->name == other.name)
+      && (this->password == other.password);
+}
+
+bool User::operator!=(const User& other) const {
+  // Checks if the users's attributes does not matches.
+  return !(*this == other);
+}
+
+User& User::operator=(const User& other) {
+  // Avoid reasignation.
+  if (this == &other) {
+    // Avoids autoassignation.
+    return *this;
+  }
+  
+  // Copy the other object attributes.  
+  this->id = other.id;
+  this->name = other.name;
+  this->permissions = other.permissions;
+  this->password = other.password;
+  
+  // Returns the actual object.
+  return *this;
+}
+
+bool User::PageAccess::operator==(const PageAccess& other) const {
+  // Checks if the page's accesses aattributes matches.
+  return (this->pageIndex == other.pageIndex)
+      && (this->access == other.access);;  
+}
+
+User::PageAccess& User::PageAccess::operator=(const PageAccess& other) {
+  // Avoid reasignation.
+  if (*this == other) {
+    return *this;
+  }
+  // Copy the other object attributes.    
+  this->pageIndex = other.pageIndex;
+  this->access = other.access;
+  // Returns the current object.
+  return *this;
+}
