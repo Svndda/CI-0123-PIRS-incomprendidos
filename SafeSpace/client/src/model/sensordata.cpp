34b58872e703cb167f49af733ce04469be9a2c73
Aaron Sanchez <aaroncz032@gmail.com>
2025-09-21 12:36:06 -0600

    feat: Definition of sensor data class.
    - This class represents the refined data received by a sensor.

---
+++ SafeSpace/client/src/model/sensordata.cpp
@@ -0,0 +1,13 @@
+#include "sensordata.h"
+
+SensorData::SensorData(
+    const int _dist, const int _mov,
+    const int _temp, const int _uv,
+    const int _micro , const int _led,
+    const int _buzzer, const int _ligth)
+    : distance(_dist), movement(_mov),
+    temperature(_temp), uv(_uv),
+    microphone(_micro), led(_led),
+    buzzer(_buzzer), ligth(_ligth) {
+  
+}
