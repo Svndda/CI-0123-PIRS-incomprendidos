40007f692fed72f7c84e2cc444301ddd4c09369d
Aaron Sanchez <aaroncz032@gmail.com>
2025-09-21 12:40:58 -0600

    feat: Logger, MessagesBoxes.
    - Definition of class functions to handle logs and messageboxes related to the page.

--- SafeSpace/client/src/view/page.cpp
+++ SafeSpace/client/src/view/page.cpp
@@ -1,3 +1,4 @@
+#include <QMessageBox>
 #include "page.h"
 
 Page::Page(QWidget *parent, Model& appModel)
@@ -7,3 +8,52 @@ Page::Page(QWidget *parent, Model& appModel)
       QString(":/images/bg1.svg"), this
   );
 }
+
+void Page::logger(const LogType type, const QString& message) {
+  // Logs the given message with the related function.
+  switch (type) {
+  case Page::DEBUG : qDebug() << message;
+    break;
+  case Page::INFO : qInfo() << message;
+    break;
+  case Page::WARNING : qWarning() << message;
+    break;
+  case Page::CRITICAL : qCritical() << message;
+    break;
+  default:
+    break;
+  }
+}
+
+void Page::logger(const LogType type, const std::string& message) {
+  this->logger(type, QString(message.data()));
+}
+
+void Page::logger(const LogType type, const char* message) {
+  this->logger(type, QString::fromUtf8(message));
+}
+
+void Page::warningMessageBox(const QString& message) {
+  // Shows a warning dialog with the given message to the user.
+  QMessageBox::warning(this, "Error", message);
+  qWarning() << message;
+}
+
+void Page::warningMessageBox(const std::string& message) {
+  this->warningMessageBox(QString(message.data()));
+}
+
+void Page::warningMessageBox(const char* message) {
+  this->warningMessageBox(QString::fromUtf8(message));
+}
+
+void Page::infoMessageBox(const QString& message) {
+  // Shows a info dialog with the given message to the user.  
+  QMessageBox::information(this, "Aviso", message);
+  qInfo() << message;
+}
+
+void Page::infoMessageBox(const char* message) {
+  this->infoMessageBox(QString::fromUtf8(message));
+}
+
--- SafeSpace/client/src/view/page.h
+++ SafeSpace/client/src/view/page.h
@@ -9,12 +9,34 @@
 
 class Page : public QWidget {
   Q_OBJECT
+  
+  // 
+  enum LogType : uint64_t {
+    DEBUG = 0,
+    INFO = 1,
+    WARNING = 2,
+    CRITICAL = 3
+  };
+  
 protected:
   QSvgRenderer *renderer;
   Model& model;
   
 public:
   explicit Page(QWidget *parent, Model& appModel);
+  
+  void logger(const LogType, const QString& message);
+  void logger(const LogType, const std::string& message);
+  void logger(const LogType, const char* message);
+  
+  void warningMessageBox(const QString& message);
+  void warningMessageBox(const std::string& message);
+  void warningMessageBox(const char* message);
+  
+  void infoMessageBox(const QString& message);
+  void infoMessageBox(const std::string& message);
+  void infoMessageBox(const char* message);
+  
 signals:
 };
 
