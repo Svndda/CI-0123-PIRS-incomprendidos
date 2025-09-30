454951604bcaa7e2354bde0a28d283baf7fc7bd6
Rolbin <rolbincm@live.comn>
2025-09-29 19:01:17 -0600

    Add example registerButton

--- SafeSpace/client/src/view/registeruserpage.h
+++ SafeSpace/client/src/view/registeruserpage.h
@@ -2,6 +2,9 @@
 #define REGISTERUSERPAGE_H
 
 #include <QWidget>
+#include <QString>
+#include <QPushButton>
+#include "UserController.h"
 
 namespace Ui {
 class RegisterUserPage;
@@ -12,12 +15,14 @@ class RegisterUserPage : public QWidget {
 public:
     explicit RegisterUserPage(QWidget *parent = nullptr);
     ~RegisterUserPage();
+    void on_registerButton_clicked();
 
 signals:
     void registerRequested(const QString &username, const QString &password, const QString &rol);
 
 private:
     Ui::RegisterUserPage *ui;
+    UserController* userController;
 };
 
 #endif // REGISTERUSERPAGE_H
