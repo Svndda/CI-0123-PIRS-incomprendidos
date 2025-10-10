e55c15bed347b8a6f9225861c37cda0c240b0bc2
David <jose.carrillosibaja@ucr.ac.cr>
2025-10-10 03:43:33 -0600

    Implementation of authentication node and testing

---
+++ SafeSpace/server/src/model/structures/AuthConstants.cpp
@@ -0,0 +1,7 @@
+#include "authenticationrequest.h"
+#include "authenticationresponse.h"
+
+constexpr std::size_t AuthRequest::USERNAME_SIZE;
+constexpr std::size_t AuthRequest::PASSWORD_SIZE;
+constexpr std::size_t AuthResponse::MESSAGE_SIZE;
+constexpr std::size_t AuthResponse::TOKEN_SIZE;
