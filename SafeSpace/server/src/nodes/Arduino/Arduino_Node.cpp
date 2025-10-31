ade1af154bbffdf990d3cd3f60316330197e6956
Aaron Sanchez <aaroncz032@gmail.com>
2025-10-30 22:49:07 -0600

    Update Arduino_Node.cpp

--- SafeSpace/server/src/nodes/Arduino/Arduino_Node.cpp
+++ SafeSpace/server/src/nodes/Arduino/Arduino_Node.cpp
@@ -174,18 +174,18 @@ void ArduinoNode::run() {
     if (!simulate && sfd >= 0) close(sfd);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cerr << "Uso: ./Arduino_Node <IP_NODO_MAESTRO> <PUERTO> [SERIAL_PATH|stdin|simulate]\n";
-        return 1;
-    }
-
-    std::string masterIP = argv[1];
-    int masterPort = std::stoi(argv[2]);
-    std::string serialPath = "";
-    if (argc >= 4) serialPath = argv[3];
-
-    ArduinoNode node(masterIP, masterPort, serialPath);
-    node.run();
-    return 0;
-}
+// int main(int argc, char* argv[]) {
+//     if (argc < 3) {
+//         std::cerr << "Uso: ./Arduino_Node <IP_NODO_MAESTRO> <PUERTO> [SERIAL_PATH|stdin|simulate]\n";
+//         return 1;
+//     }
+//
+//     std::string masterIP = argv[1];
+//     int masterPort = std::stoi(argv[2]);
+//     std::string serialPath = "";
+//     if (argc >= 4) serialPath = argv[3];
+//
+//     ArduinoNode node(masterIP, masterPort, serialPath);
+//     node.run();
+//     return 0;
+// }
